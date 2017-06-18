d2ae904b2e788cd365e774a38418d14dc8cf3b5f
ramiro <ramihertz@gmail.com>
2017-06-17 21:28:28 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
+    <quote></quote>
   </div>
 </template>
 
 <script>
+import Quote from './components/Quotes.vue'
 export default {
+  components:{
+    Quote
+  }
 }
 </script>
 
---
+++ src/components/Quotes.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      A wonderful quote goes here.
+  </div>
+</template>
+<script>
+    
+</script>
+<style scoped>
+div {
+    padding: 20px;
+    border: 1px dotted green;
+    text-align: center;
+    margin: 30px auto;
+    font-style: italic;
+    font-size: 20px;
+    font-weight: bold;
+}
+</style>
