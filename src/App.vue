140f9feb77d02fb92555e645c113ae556a42017f
ramiro <ramihertz@gmail.com>
2017-06-18 18:27:09 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <div id="app">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <h2>Built-in directives</h2>
+        <p v-text="'some radom text'"></p>
+      </div>
+    </div>
+
+        <div class="row">
+      <div class="col-xs-12">
+        <h2>Custom directives</h2>
+        <!--<p v-highlight>mierda</p>-->
+        <p v-highlight="'red'">mierda</p>
+        <p v-local-highlight>carajo</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+  export default {
+    directives:{
+      'local-highlight':{
+        bind(el, binding, vnode){
+          el.style.backgroundColor = 'yellow'
+        }
+      }
+    }
+  }
+
 </script>
 
 <style>
