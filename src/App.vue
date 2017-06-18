4231ca5fa26d3e8e88eb024e3f4fc0dfcecc49ee
ramiro <ramihertz@gmail.com>
2017-06-18 19:34:34 -0400

    using computed over filters

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
     <p>
       {{text | upperCase | to-lowercase}}
     </p>
+    <p>
+      <input type="" name="" value="" v-model="searchFruit">
+    </p>
+    <p>
+      <ul>
+        <li v-for="fruit in filterFruits">
+          {{fruit}}
+        </li>
+      </ul>
+    </p>
   </div>
 </template>
 
@@ -10,12 +20,25 @@
 export default {
   data(){
     return {
-      text: 'Hola carajo!'
+      text: 'Hola carajo!',
+      fruits: [
+        'apple',
+        'banana',
+        'strawberry',
+        'mango',
+        'melon',
+      ],
+      searchFruit:""
     }
   },
   filters:{
     upperCase(value){
       return value.toUpperCase()
+    }
+  },
+  computed:{
+    filterFruits(){
+      return this.fruits.filter(x => x.match(this.searchFruit))
     }
   }
 }
