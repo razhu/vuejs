2c6c56234b8da96073d13e1462ce2718cbc55573
ramiro <ramihertz@gmail.com>
2017-06-18 10:11:27 -0400

    mi solucion

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
+    <quote></quote>
   </div>
 </template>
 
 <script>
+  import Quote from './components/Quote.vue'
 export default {
+  components:{
+    Quote
+  }
 }
 </script>
 
---
+++ src/components/Quote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h3>Enter quote below</h3>
+        <input type="text" name="" value="" v-model="quote">
+        <button type="" @click="addQuote">Add quote</button>
+        <hr>
+        <ul>
+            <li v-for="quote in quotes">{{quote}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                quote: '',
+                quotes: []
+            }
+        },
+        methods: {
+            addQuote() {
+                this.quotes.push(this.quote)
+            }
+        }
+    }
+
+</script>
+<style scoped>
+    input {
+        width: 300px;
+        height: 50px
+    }
+</style>
