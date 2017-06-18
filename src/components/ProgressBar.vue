1a3fc8c8888e95a876464f8e41d71c471cdf4ede
ramiro <ramihertz@gmail.com>
2017-06-18 14:18:09 -0400

    ok

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="outdiv"> 
       <div class="progress"></div>
-      
+      {{quoteNumberBar}}
   </div>
 </template>
 <script>
-    
+
+    import { eventBus } from '../main'
+    export default {
+        data(){
+            return {
+                quoteNumberBar: 0
+            }
+        }, 
+        created(){
+            eventBus.$on('quoteNumber', number =>{
+                this.quoteNumberBar = number
+            })
+        }
+    }
 </script>
 <style scoped>
 div{
--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-      <quote v-for="(quote, index) in arrayQuotes" @click.native='deleteQuote(quote)' :theIndex=index>{{quote}} {{index+1}}</quote>
-  </div>
+    <div>
+        <quote v-for="(quote, index) in arrayQuotes" @click.native='deleteQuote(quote)' v-model='quoteNumber = index +1'>{{quote}} {{index+1}}</quote>
+    </div>
 </template>
 <script>
-import Quote from './Quote.vue'
-import {eventBus} from '../main'
-    export default{
-        data(){
+    import Quote from './Quote.vue'
+    import { eventBus } from '../main'
+    export default {
+        data() {
             return {
-                numberQuotes : 10,
+                numberQuotes: 10,
                 arrayQuotes: [],
                 quoteNumber: 0
             }
@@ -17,18 +17,21 @@ import {eventBus} from '../main'
         components: {
             Quote
         },
-        methods:{
-            deleteQuote(quote){
+        methods: {
+            deleteQuote(quote) {
                 this.arrayQuotes.splice(this.arrayQuotes.indexOf(quote), 1)
             }
         },
         props: ['aQuote'],
-        created(){
-            eventBus.$on('emittedQuote', quoteToAdd =>{
+        created() {
+            eventBus.$on('emittedQuote', quoteToAdd => {
                 this.arrayQuotes.push(quoteToAdd)
             })
+            eventBus.$emit('quoteNumber', this.quoteNumber)
+
         }
     }
+
 </script>
 <style>
 
