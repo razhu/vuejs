5a76bec8571b4d323ab792bb8fbe8ff641da197a
ramiro <ramihertz@gmail.com>
2017-06-18 23:30:44 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <template>
-  <div id="app">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label for="">Username</label>
+          <input type="text" class="form-control " v-model="user.username">
+        </div>
+        <div class="form-group">
+          <label for="">Email</label>
+          <input type="text" class="form-control" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+      </div>
+      
+    </div>
+    <div class="row">
+      <p>{{message}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+  export default {
+    data(){
+      return {
+        user:{
+          username:'',
+          email:''
+        },
+        message: ''
+      }
+    },
+    methods:{
+      submit(){ 
+        this.$http.post('https://vuejs-http-a9715.firebaseio.com/data.json', this.user)
+        .then(respu => {
+          this.message ='user sent and saved.'
+          console.log(respu)
+        })
+        .catch(err => {
+          this.message = err
+        })
+      }
+    }
+  }
+
 </script>
 
 <style>
