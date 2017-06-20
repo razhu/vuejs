3a240a702922bf2be72e5430dc40115dcf611d13
ramiro <ramihertz@gmail.com>
2017-06-19 21:54:41 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="app">
+<!-- Columns are always 50% wide, on mobile and desktop -->
+<div class="container">
+<div class="row">
+  <div class="col-6">.col-6</div>
+  <div class="col-6">.col-6</div>
+</div>
   </div>
+
 </template>
 
 <script>
