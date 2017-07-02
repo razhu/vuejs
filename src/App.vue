b989e539c37ddf3ee0d8a0a02079a00ee332a62e
ramiro <ramiro@ramiros-MacBook-Pro.local>
2017-07-02 13:16:37 -0400

    sdaf

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
-  <div id="app">
+  <!-- Columns are always 50% wide, on mobile and desktop -->
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <my-header></my-header>
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
-export default {
-}
+  import Header from './components/Header.vue'
+  export default {
+    components: {
+      myHeader: Header
+    }
+  }
+
 </script>
 
 <style>
+  body {
+    padding: 30px;
+  }
 
 </style>
